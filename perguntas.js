417588f9c5021185082a2264abdce25cad60bdd4
lauralejaricob <160238139+lauralejaricob@users.noreply.github.com>
2024-10-07 00:02:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao (
+    'Medicina',
+    'Normalmente, quantos litros de sangue uma pessoa tem?',
+    'Tem entre 4 a 6 litros'
+)
+
+criaCartao (
+    'Matemática',
+    'Quantas casas decimais tem o número pi?',
+    'Infinitas'
+)
+
+criaCartao (
+    'Quimica',
+    'Em que ordem surgiram os modelos atômicos?',
+    'Dalton, Thomson, Rutherford, Rutherford-Bohr.'
+)
